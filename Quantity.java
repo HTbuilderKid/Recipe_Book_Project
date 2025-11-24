642fcdb6d00c8116dea2c0b0ae61b654744cb232
CYW <chenyuw2019@gmail.com>
2025-11-24 15:10:09 -0500

    Updated the basic project structure

---
+++ Quantity.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Quantity here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Quantity extends Ingredients
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Quantity
+     */
+    public Quantity()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
