8a5aefc79caa973cfc64beeee253926f02c731fb
CYW <chenyuw2019@gmail.com>
2025-11-24 15:39:46 -0500

    Removed the arrows used to signal the relationship between classes (I added actual inheritence between the RecipeCold/Hot and the Recipes, so the arrows have become confusing)

--- Recipes.java
+++ Recipes.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Recipes extends RecipeBook
+public class Recipes
 {
     // instance variables - replace the example below with your own
     private int x;
