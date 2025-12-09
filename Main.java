4b6cc9fc77f9a01aeb9c6c5a5e591dc14acd0677
CYW <chenyuw2019@gmail.com>
2025-12-09 12:14:51 -0500

    Added the Main class which uses the logic in RecipeAdder to create a new Recipe and add it to RecipeBook

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (Chenyu)
+ * @version (Elon Musk)
+ */
+public class Main {
+    public static void main(String[] args) {
+        RecipeBook book = new RecipeBook("My Recipes", "Gigachad");
+        RecipeAdder adder = new RecipeAdder(book);
+        
+        System.out.println("=== Welcome to GigaMan's Recipe Book Creator! ===");
+        adder.createAndAddRecipe();
+        System.out.println("\n=== Your Recipe has been added! ===");
+        book.listAllRecipes();
+    }
+}
